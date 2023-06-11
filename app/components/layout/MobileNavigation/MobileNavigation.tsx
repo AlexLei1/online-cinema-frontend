f84aad04c9529cbab9b03f9aae6e1b1b2558ef29
Alex Lei <sasha.volkov2015@yandex.ru>
2023-06-12 02:55:56 +0300

    updated MobileNavigation.tsx file

--- app/components/layout/MobileNavigation/MobileNavigation.tsx
+++ app/components/layout/MobileNavigation/MobileNavigation.tsx
@@ -2,15 +2,19 @@ import { FC } from 'react'
 import MenuContainer from './MenuContainer/MenuContainer'
 import styles from './MobileNavigation.module.scss'
 import cn from 'classnames';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
+import useActions  from '@/hooks/useActions';
 
 interface IMobileNavigation {
 	isShow: boolean
 }
 
 const MobileNavigation: FC<IMobileNavigation> = ({isShow}) => {
-	
+	const {toggleBurger} = useActions()
+	const {ref} = useOnClickOutside(toggleBurger, isShow)
+
 	return (
-		<div hidden={isShow} className={cn(styles.navigation)}>
+		<div ref={ref} hidden={isShow} className={cn(styles.navigation)}>
 			<MenuContainer />
 		</div>
 	)

25034986cb4f7a6c166e21f84c91462911635c7e
Alex Lei <sasha.volkov2015@yandex.ru>
2023-06-26 06:45:04 +0300

    edit form component and login page

--- app/components/ui/form-elements/Field.tsx
+++ app/components/ui/form-elements/Field.tsx
@@ -8,10 +8,10 @@ import styles from './form.module.scss'
 const Field = forwardRef<HTMLInputElement, IField>(
 	({ placeholder, error, type = 'text', style, ...rest }, ref) => {
 		return (
-			<div className={cn(styles.common, styles.field)} style={style}>
+			<div className={cn(styles.field, styles.common)} style={style}>
 				<label>
 					<span>{placeholder}</span>
-					<input ref={ref} type={type} {...rest} />
+					<input className={styles.common} ref={ref} type={type} {...rest} />
 				</label>
 				{error && <div className={styles.error}>{error.message}</div>}
 			</div>
